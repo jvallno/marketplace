1c13d87ea1ff3be9e75292017f3d0241242e964b
unknown <jurjani.valles@gmail.com>
2022-03-05 18:54:13 +0800

    Added the product page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Home} from './pages/home/home.component';
+import { Products } from './pages/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Home
+    Home,
+    Products
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class Products implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
